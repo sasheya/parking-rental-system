7eebce7bd8a6c0e9cc2b9673a7bb017f928a2232
sasheya <shivani.asheya@gmail.com>
2026-09-08 14:34:39 +0530

    Implement common-security

--- common-security/src/main/java/com/parking/common_security/JwtAuthenticationFilter.java
+++ common-security/src/main/java/com/parking/common_security/JwtAuthenticationFilter.java
@@ -1,5 +1,46 @@
 package com.parking.common_security;
 
-public class JwtAuthenticationFilter {
+import java.util.List;
 
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.List;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+	private final JwtValidator jwtValidator;
+	
+	public JwtAuthenticationFilter(JwtValidator jwtValidator) {
+		this.jwtValidator = jwtValidator;
+	}
+	
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,  FilterChain filterChain) throws IOException, ServletException {
+		String header = request.getHeader("Authorization");
+		if(header != null && header.startsWith("Bearer ")) {
+			String token = header.substring(7);
+			try {
+				Long userId = jwtValidator.getUserId(token);
+				String role = jwtValidator.getRole(token);
+				
+				var authentication = new UsernamePasswordAuthenticationToken(userId, null, List.of(new SimpleGrantedAuthority("ROLE" + role)));
+				SecurityContextHolder.getContext().setAuthentication(authentication);
+			} catch(Exception e) {
+				SecurityContextHolder.clearContext();
+			}
+		}
+		
+		filterChain.doFilter(request, response);
+	}
+	
 }
