31133091bfee1ce32e20d16eec3005140634cb32
sasheya <shivani.asheya@gmail.com>
2026-09-10 17:30:51 +0530

    fixed swagger implementation

---
+++ booking-service/src/main/java/com/parking/booking_service/config/OpenApiConfig.java
@@ -0,0 +1,43 @@
+package com.parking.booking_service.config;
+
+
+import java.util.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+ 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+ 
+@Configuration
+public class OpenApiConfig {
+ 
+    @Bean
+    public OpenAPI customOpenAPI() {
+ 
+        final String securitySchemeName = "bearerAuth";
+ 
+        return new OpenAPI()
+                .servers(List.of(
+                        new Server().url("/")
+                ))
+                .addSecurityItem(
+                        new SecurityRequirement()
+                                .addList(securitySchemeName)
+                )
+                .components(
+                        new Components()
+                                .addSecuritySchemes(
+                                        securitySchemeName,
+                                        new SecurityScheme()
+                                                .name(securitySchemeName)
+                                                .type(SecurityScheme.Type.HTTP)
+                                                .scheme("bearer")
+                                                .bearerFormat("JWT")
+                                )
+                );
+    }
+}
+ 
